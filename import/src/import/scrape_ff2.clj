bb0408f2ff56e46da557e2ad53b78fdf5e7abe1b
Juhani Hietikko <juhani.hietikko@gmail.com>
2014-05-31 11:17:28 +0300

    Can import from topics that hold multiple matches combined in each table.

--- import/src/import/scrape_ff2.clj
+++ import/src/import/scrape_ff2.clj
@@ -3,21 +3,25 @@
 
 (defn- parse-table [html-tables n map-fn]
   (let [rows (map map-fn (html/select (nth html-tables n) [:tr]))]
-    (reduce (fn [rows-map row] 
-              (conj rows-map {(:player-name row) row})) 
+    (reduce (fn [rows-map row]
+              (let [player (:player-name row)]
+                (if (empty? player)
+                  rows-map
+                  (conj rows-map {(:player-name row) row})))) 
             {} 
             rows)))
 
-(defn- parse-row [html-row cols]
+(defn- parse-row [html-row cols offset]
   (let [cells (html/select html-row [:td])
         filtered-cells (if (= 9 (count cells))
                          ; A hack to handle the variation of the passes table 
                          ; where there are extra columns for both halves.
                          [(nth cells 1) (nth cells 2) (nth cells 6)]
                          (rest cells))
+        cells-with-offset (drop (* offset (count cols)) filtered-cells)
         map-seed {:player-name (html/text (first cells))}]
     (loop [remaining-cols cols
-           remaining-cells filtered-cells
+           remaining-cells cells-with-offset
            the-map map-seed]
       (if (or (empty? remaining-cols) (empty? remaining-cells))
         the-map
@@ -25,24 +29,23 @@
                (rest remaining-cells)
                (conj the-map {(first remaining-cols) (html/text (first remaining-cells))}))))))
 
-(defn- parse-minutes-and-passes-row [html-row]
-  (parse-row html-row [:minutes :all-passes :hard-passes]))
+(defn- row-parser [cols offset]
+  (fn [html-row]
+    (parse-row html-row cols offset)))
 
-(defn- parse-attack-duels-row [html-row]
-  (parse-row html-row [:other-duels-attack :aerial-duels-attack]))
-
-(defn- parse-defence-duels-row [html-row]
-  (parse-row html-row [:other-duels-defence :aerial-duels-defence]))
-
-(defn- parse-misc-row [html-row]
-  (parse-row html-row [:interceptions :dribbles :ball-holdings :goal-attempts-and-assists]))
-
-(defn match-stats-from-post [html-post]
+(defn match-stats-from-post [html-post match-index]
   (let [html-tables (html/select html-post [:table])
-        minutes-and-passes (parse-table html-tables 0 parse-minutes-and-passes-row)
-        attack-duels (parse-table html-tables 1 parse-attack-duels-row)
-        defence-duels (parse-table html-tables 2 parse-defence-duels-row)
-        misc (parse-table html-tables 3 parse-misc-row)]
+        minutes-and-passes (parse-table html-tables 0 (row-parser [:minutes 
+                                                                   :all-passes 
+                                                                   :hard-passes] match-index))
+        attack-duels (parse-table html-tables 1 (row-parser [:other-duels-attack 
+                                                             :aerial-duels-attack] match-index))
+        defence-duels (parse-table html-tables 2 (row-parser [:other-duels-defence 
+                                                              :aerial-duels-defence] match-index))
+        misc (parse-table html-tables 3 (row-parser [:interceptions 
+                                                     :dribbles 
+                                                     :ball-holdings 
+                                                     :goal-attempts-and-assists] match-index))]
     (merge-with (fn [row-map-a row-map-b]
                   (merge row-map-a row-map-b)) 
                 minutes-and-passes attack-duels defence-duels misc)))
@@ -53,5 +56,8 @@
 (defn- opening-post-in-topic [topic-url]
   (first (html/select (fetch-url topic-url) [:div.post])))
 
-(defn match-stats-from-topic [topic-url]
-  (match-stats-from-post (opening-post-in-topic topic-url)))
+(defn match-stats-from-topic 
+  ([topic-url]
+   (match-stats-from-topic topic-url 0))
+  ([topic-url match-index]
+   (match-stats-from-post (opening-post-in-topic topic-url) match-index)))
