b753d388082569e1ad4b3caf6b7223cdb8c6ac40
Juhani Hietikko <juhani.hietikko@gmail.com>
2014-06-10 22:55:11 +0300

    Filtering some of the oldest match topic that have different format from the newer ones.

--- import/src/import/core.clj
+++ import/src/import/core.clj
@@ -6,6 +6,9 @@
 
 (defn -main
   [& args]
-  (println (matches/pick-match-links-from "http://futisforum2.org/index.php?topic=126157.0")))
+  (let [match-topic-urls (matches/pick-match-links-from 
+                           "http://futisforum2.org/index.php?topic=126157.0")
+        stats-per-match (map ff2/match-stats-from-topic match-topic-urls)]
+    (println stats-per-match)))
   ;(mg-import/save-to-mongo {})
   ;(println (ff2/match-stats-from-topic "http://futisforum2.org/index.php?topic=159803.0" 0)))
--- import/src/import/ff2_match_search.clj
+++ import/src/import/ff2_match_search.clj
@@ -6,7 +6,11 @@
   (let [links-in-opening-post (html/select (util/opening-post-in-topic url) [:a])
         links-to-drop #{"http://futisforum2.org/index.php?topic=16119.0"
                         "http://futisforum2.org/index.php?topic=41553.0"
-                        "http://futisforum2.org/index.php?topic=157398.0"}]
+                        "http://futisforum2.org/index.php?topic=157398.0"
+                        "http://futisforum2.org/index.php?topic=48147.0"
+                        "http://futisforum2.org/index.php?topic=39582.0"
+                        "http://futisforum2.org/index.php?topic=36265.0"
+                        "http://futisforum2.org/index.php?topic=28487.0"}]
     (->> (map #(get-in % [:attrs :href]) 
               links-in-opening-post)
          (filter (complement links-to-drop)))))
